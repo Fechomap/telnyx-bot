8770add5d97789a4b2742239d6ec48528c2adc09
Fechomap <jhon_free27@hotmail.com>
2025-01-31 23:38:38 -0600

    Corrección de API Key y subida de dependencias

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
+const { OpenAIApi, Configuration } = require("openai");
 
 const app = express();
 app.use(express.json());
